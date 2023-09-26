028f4b80d0f509ecc3896a033b3d7779efbbdffa
Saif-09 <saifmd238@gmail.com>
2023-09-26 16:04:09 +0530

    useEffect setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { TodoProvider } from "./context"
 
 
@@ -21,6 +21,20 @@ function App() {
   const toggleTodo = (id) =>{
     setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id === id ?{...prevTodo,isCompleted:!prevTodo.isCompleted}:prevTodo))
   }
+
+  useEffect(()=>{
+    
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+
+  },[])
+
+  useEffect(()=>{
+      localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodo}}>
